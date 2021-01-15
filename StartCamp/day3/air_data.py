a1764a81d811deaaafc8b7414e1f6d2cc97b061b
Sohyun Park <mymysuzy0627@gmail.com>
2021-01-15 16:43:42 +0900

    토큰을 덜 지워서 다시 업로드함

--- StartCamp/day3/air_data.py
+++ StartCamp/day3/air_data.py
@@ -3,9 +3,9 @@ import requests, pprint
 
 
 # key 부분에는 일반 인증키 (UTF-8)가 들어간다.
-key = 'qHal4sXbiIqxF1lwiVdTkedQt4zt8QN0VmgggXnoeAcltdUGsTCINv0AaH9gYVf9rAYN9XsaeNe4XDc4N4DX0A%3D%3D'
+key = ''
 # url 부분에는 End point 값이 들어가며 제일 뒷 부분에 ?service={key}를 추가해 준다.
-url = f'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?service={key}&sidoName=광주&returnType=json'
+url = f''
 
 response = requests.get(url).json()
 # pprint.pprint(response)
@@ -18,8 +18,8 @@ print(f'{sido_name}의 미세먼지 농도는 {pm_10}입니다. ( 측성소 : {s
 
 # 텔레그램 메세지 전송
 
-token = token = '1516249307:AAHVIGT52mHDQuP-bo6bDgfPK5QP-7mVKv8'
-chat_id = '1536127633'
+token = token = ''
+chat_id = ''
 
 
 telegram_url = f'https://api.telegram.org/bot/sendMessage?chat_id={chat_id}&text={text}'
